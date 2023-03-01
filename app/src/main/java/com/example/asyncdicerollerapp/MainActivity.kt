a5eb1aaacd88a0c97c1c696787d568dbdd0f3a9c
walid <walid@waled.com>
2023-03-01 19:58:18 +0100

    clase 01/03

--- app/src/main/java/com/example/asyncdicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/asyncdicerollerapp/MainActivity.kt
@@ -2,39 +2,62 @@ package com.example.asyncdicerollerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import com.example.asyncdicerollerapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    private val diceList = listOf(R.drawable.dice_1,R.drawable.dice_2,R.drawable.dice_3,R.drawable.dice_4,R.drawable.dice_5,R.drawable.dice_6)
+    private lateinit var binding: ActivityMainBinding
+    private val diceList = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.dice1.setOnClickListener {
-            CoroutineScope(Dispatchers.Default).launch {
-                val index = getRandomNum()
-                while (index)
-                binding.dice1.setImageResource(diceList[])
-            }
-        }
-        binding.dice2.setOnClickListener {
-            binding.dice2.setImageResource(diceList[getRandomNum()])
+        binding.launchButton.setOnClickListener {
+            val order = mutableListOf(
+                    launchCoroutine(binding.dice1),
+                    launchCoroutine(binding.dice2),
+                    launchCoroutine(binding.dice3),
+                    launchCoroutine(binding.dice4))
 
         }
-        binding.dice3.setOnClickListener {
-            binding.dice3.setImageResource(diceList[getRandomNum()])
-        }
-        binding.dice4.setOnClickListener {
-            binding.dice4.setImageResource(diceList[getRandomNum()])
-        }
     }
-    private suspend fun getRandomNum(): Int {
+
+    private suspend fun getRandomDeferredNum(): Deferred<Int> {
         val value = CoroutineScope(Dispatchers.Default).async {
             delay((500..3000).random().toLong())
-            (0..5).random()
+            getRandomNum()
         }
-        return value.await()
+        return value
+    }
+
+    private fun getRandomNum(): Int {
+        return (0..5).random()
+    }
+
+    private fun launchCoroutine(imageView: ImageView): Deferred<Unit> {
+        return CoroutineScope(Dispatchers.Default).async {
+            val index = getRandomDeferredNum()
+            while (index.isActive) {
+                updateUI(imageView)
+            }
+            imageView.setImageResource(diceList[index.await()])
+        }
+    }
+
+    private suspend fun updateUI(imageView: ImageView) {
+        withContext(Dispatchers.Main) {
+            imageView.setImageResource(diceList[getRandomNum()])
+        }
     }
 }
